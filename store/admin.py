1cb60d2fd41ef59a2f0faf61cd12cbdcdd244eab
AdirNoyman <ofahol2007@hotmail.com>
2023-02-20 23:03:52 +0200

    Added more html views

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . models import Category, Product
+# We need to register this models for our admin page, so we could see them there Here we are also pre-populating are
+# slug field with the name we entered for the category or product we will create. That way also making sure they are
+# the same
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+
--- store/models.py
+++ store/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Category(models.Model):
     # We index the category name so the DB querys will be more efficent
     name = models.CharField(max_length=250, db_index=True)
-    # The category URI
+    # The category URL enpoint
     slug = models.SlugField(max_length=250, unique=True)
 
     # set how the plural of Category plural name, explictly, for the admin
@@ -23,10 +23,10 @@ class Product(models.Model):
     brand = models.CharField(max_length=250, default='un-branded')
     # 'TextField' - enables entering more text. and we want to set to optional so that wy we have set it to 'blank=True'
     description = models.TextField(blank=True)
-    # The brand URI
+    # The product URL enpoint
     slug = models.SlugField(max_length=250)
     price = models.DecimalField(max_digits=4, decimal_places=2)
-    # image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/',blank=True, null=True)
     # set how the plural of Category plural name, explictly, for the admin
 
     class Meta:
